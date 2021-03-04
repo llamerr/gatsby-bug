65edd067ef726d79e6548b2b84b47f951ee8c123
Alexey Lukin <alexey.lukin@da-14.com>
2021-03-04 18:36:28 +0200

    reproduce bug

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,6 +1,6 @@
 import React from "react"
 
-import * as styles from './index.module.scss'
+import * as styles from './404.module.scss'
 
 export default function Home() {
   return <div className={styles.test}>Hello world!</div>
